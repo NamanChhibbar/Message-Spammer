dea0fed077ffdf28edbd1c14e082bbe596d57041
Naman Chhibbar <n.chhibar@gmail.com>
2024-10-04 13:33:33 +0530

    Updated documentation

--- spammer.py
+++ spammer.py
@@ -4,18 +4,23 @@ import pyautogui as pt
 
 random.seed(0)
 
-def spam(messages: list[str], num: int, gap: int=5):
-    """
-    Types and enters a random message from the given list of messages with
-    specified time gap. Move mouse cursor to top left corner to stop.
+def spam(
+  messages: list[str],
+  num: int,
+  gap: int=5
+) -> None:
+  """
+  Types and enters a random message from the given list of messages with
+  specified time gap.
+  Move mouse cursor to top left corner to stop.
 
-    ## Parameters
-    `messages`: `list` containing messages to type
-    `num`: Number of messages to type
-    `gap`: Time gap in seconds between messages
-    """
-    sleep(gap)
-    for _ in range(num):
-        message = random.choice(messages)
-        pt.typewrite(message)
-        pt.press("enter")
+  :param messages: list of messages to type
+  :param num: Number of messages to type
+  :param gap: Time gap in seconds between messages
+  """
+
+  sleep(gap)
+  for _ in range(num):
+    message = random.choice(messages)
+    pt.typewrite(message)
+    pt.press("enter")
